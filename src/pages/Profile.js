fa72345c425a5ff112de1ed77eaf0df0f6bc0b66
Po-Hao KO <oldgee52@gmail.com>
2022-04-17 16:43:24 +0800

    refactor input function with component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import firebaseInit from "../utils/firebase";
 import styled from "styled-components";
 import { updateDoc, doc } from "firebase/firestore";
+import { InputForModify } from "../Component/InputForModify";
 
 const Container = styled.div`
     margin: auto;
@@ -35,7 +36,6 @@ export const Profile = () => {
     const studentID = "WBKPGMSAejc9AHYGqROpDZWWTz23";
     useEffect(() => {
         firebaseInit.getCollectionData("users", studentID).then(data => {
-            console.log(data);
             setUserInfo(data);
             SetInputFields({
                 name: data.name,
@@ -43,65 +43,6 @@ export const Profile = () => {
             });
         });
     }, []);
-
-    function handleInputChange(e) {
-        let data = { ...inputFields };
-        data[e.target.name] = e.target.value;
-        SetInputFields(data);
-    }
-
-    // async function handleModifyClick() {
-    //     if (modifyUserName) {
-    //         setModifyUserName(false);
-    //     }
-    //     if (!modifyUserName) {
-    //         await updateDoc(doc(firebaseInit.db, "users", studentID), {
-    //             name: inputFields.name,
-    //         });
-    //         setUserInfo(prve => ({
-    //             ...prve,
-    //             name: inputFields.name,
-    //         }));
-    //         setModifyUserName(true);
-    //     }
-    // }
-
-    // function handleCancelModify() {
-    //     setModifyUserName(true);
-    //     SetInputFields(prve => ({
-    //         ...prve,
-    //         name: userInfo.name,
-    //     }));
-    // }
-
-    async function handleModifyClick(state, modifyCallback, modifyContent) {
-        if (state) {
-            modifyCallback(false);
-        }
-        if (!state) {
-            const modifyData = {
-                [`${modifyContent}`]: inputFields[modifyContent],
-            };
-            console.log(modifyData);
-            await updateDoc(
-                doc(firebaseInit.db, "users", studentID),
-                modifyData,
-            );
-            setUserInfo(prve => ({
-                ...prve,
-                [`${modifyContent}`]: inputFields[modifyContent],
-            }));
-            modifyCallback(true);
-        }
-    }
-
-    function handleCancelModify(modifyCallback, modifyContent) {
-        modifyCallback(true);
-        SetInputFields(prve => ({
-            ...prve,
-            [`${modifyContent}`]: userInfo[modifyContent],
-        }));
-    }
     return (
         <Container>
             <div>Profile</div>
@@ -116,70 +57,32 @@ export const Profile = () => {
                             height="50"
                         ></img>
                     </Div1>
-                    <Div1>
-                        <DivTitle>姓名</DivTitle>
-                        <Input
-                            value={inputFields?.name || ""}
-                            disabled={modifyUserName}
-                            name="name"
-                            onChange={e => handleInputChange(e)}
-                        ></Input>
-                        <button
-                            onClick={() =>
-                                handleModifyClick(
-                                    modifyUserName,
-                                    setModifyUserName,
-                                    "name",
-                                )
-                            }
-                        >
-                            {modifyUserName ? "修改" : "確定"}
-                        </button>
-                        {modifyUserName || (
-                            <button
-                                onClick={() => {
-                                    handleCancelModify(
-                                        setModifyUserName,
-                                        "name",
-                                    );
-                                }}
-                            >
-                                取消
-                            </button>
-                        )}
-                    </Div1>
-                    <Div1>
-                        <DivTitle>自我介紹</DivTitle>
-                        <Input
-                            value={inputFields?.selfIntroduction || ""}
-                            name="selfIntroduction"
-                            onChange={e => handleInputChange(e)}
-                            disabled={modifyUserIntroduction}
-                        ></Input>
-                        <button
-                            onClick={() =>
-                                handleModifyClick(
-                                    modifyUserIntroduction,
-                                    setModifyUserIntroduction,
-                                    "selfIntroduction",
-                                )
-                            }
-                        >
-                            {modifyUserIntroduction ? "修改" : "確定"}
-                        </button>
-                        {modifyUserIntroduction || (
-                            <button
-                                onClick={() => {
-                                    handleCancelModify(
-                                        setModifyUserIntroduction,
-                                        "selfIntroduction",
-                                    );
-                                }}
-                            >
-                                取消
-                            </button>
-                        )}
-                    </Div1>
+                    {inputFields && (
+                        <>
+                            <InputForModify
+                                inputFields={inputFields}
+                                SetInputFields={SetInputFields}
+                                studentID={studentID}
+                                userInfo={userInfo}
+                                setUserInfo={setUserInfo}
+                                handleDisable={modifyUserName}
+                                setHandleDisable={setModifyUserName}
+                                title="姓名"
+                                targetName="name"
+                            />
+                            <InputForModify
+                                inputFields={inputFields}
+                                SetInputFields={SetInputFields}
+                                studentID={studentID}
+                                userInfo={userInfo}
+                                setUserInfo={setUserInfo}
+                                handleDisable={modifyUserIntroduction}
+                                setHandleDisable={setModifyUserIntroduction}
+                                title="自我介紹"
+                                targetName="selfIntroduction"
+                            />
+                        </>
+                    )}
                 </>
             )}
         </Container>
