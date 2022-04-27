43dd82fe23cb280d977c7d20a4bc910d60b83b58
Po-Hao KO <oldgee52@gmail.com>
2022-04-27 18:25:06 +0800

    finish banner and search  area

--- src/Component/SearchInput.js
+++ src/Component/SearchInput.js
@@ -1,12 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import { FiSearch } from "react-icons/fi";
+import { breakPoint } from "../utils/breakPoint";
 
-const InputArea = styled.input`
+const SearchFrom = styled.form`
     width: 100%;
+    display: flex;
+    justify-content: center;
+    @media ${breakPoint.desktop} {
+        justify-content: flex-start;
+        padding-left: 10%;
+    }
 `;
 
-const Button = styled.button`
-    width: 100%;
+const InputArea = styled.input`
+    width: 80%;
+    height: 30px;
+    background: whitesmoke;
+    border: 2px solid black;
+    border-left: none;
+    color: rgba(0, 0, 0, 0.65);
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+
+    font-weight: 600;
+
+    &:focus {
+        outline: none;
+    }
+
+    @media ${breakPoint.desktop} {
+        width: 350px;
+    }
+`;
+
+const Search = styled(FiSearch)`
+    width: 30px;
+    height: 30px;
+
+    border: 2px solid black;
+    background-color: whitesmoke;
+    border-right: none;
+
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    cursor: pointer;
 `;
 
 export const SearchInput = ({
@@ -15,14 +53,15 @@ export const SearchInput = ({
     searchCallback,
 }) => {
     return (
-        <>
+        <SearchFrom onSubmit={searchCallback}>
+            <Search viewBox="-5 -5 35 35" onClick={searchCallback} />
+
             <InputArea
                 type="search"
-                placeholder="Search"
+                placeholder="今天想學習什麼?"
                 value={searchField}
                 onChange={changeValueCallback}
             />
-            <Button onClick={searchCallback}>送出</Button>
-        </>
+        </SearchFrom>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import firebaseInit from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { SearchInput } from "../Component/SearchInput";
+import { breakPoint } from "../utils/breakPoint";
 
 const Container = styled.div`
     margin: auto;
@@ -12,7 +13,13 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
-    width: 1000px;
+    width: 100%;
+`;
+
+const Banner = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color: #ff6100;
 `;
 
 const Div1 = styled.div`
@@ -29,9 +36,30 @@ const Title = styled.h2`
     justify-content: center;
 `;
 
-const DivFlex = styled.div`
+const InputArea = styled.div`
+    top: 250px;
+    width: 100%;
     display: flex;
-    justify-content: flex-end;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    @media ${breakPoint.desktop} {
+        top: 200px;
+    }
+`;
+const BannerTitle = styled.div`
+    font-size: 20px;
+    color: black;
+    font-weight: 700;
+    text-align: center;
+    flex: 1 0 100%;
+    margin-bottom: 20px;
+
+    @media ${breakPoint.desktop} {
+        text-align: left;
+        padding-left: 10%;
+    }
 `;
 
 export const Home = () => {
@@ -64,61 +92,71 @@ export const Home = () => {
     }, []);
 
     return (
-        <Container>
-            {!latestCourse || !popularCourse ? (
-                "loading..."
-            ) : (
-                <Div1>
+        <>
+            <Banner>
+                <InputArea>
+                    <BannerTitle>今晚要來點什麼?</BannerTitle>
                     <SearchInput
                         searchField={searchField}
-                        changeValueCallback={e =>
-                            setSearchField(e.target.value)
-                        }
-                        searchCallback={() => {
+                        setSearchField={setSearchField}
+                        changeValueCallback={e => {
+                            e.preventDefault();
+                            setSearchField(e.target.value);
+                        }}
+                        searchCallback={e => {
+                            e.preventDefault();
                             if (!searchField.trim()) return;
                             navigate(`/search?q=${searchField.trim()}`);
                         }}
                     />
-                    <Title>最新上架</Title>
-                    <DivFlex>
-                        {latestCourse.map(course => (
-                            <CourseInfo
-                                key={course.courseID}
-                                courseID={course.courseID}
-                                title={course.title}
-                                teacherName={course.teacherInfo.name}
-                                creatDate={new Date(
-                                    course.creatTime.seconds * 1000,
-                                ).toLocaleDateString()}
-                                openingDate={new Date(
-                                    course.openingDate.seconds * 1000,
-                                ).toLocaleDateString()}
-                            />
-                        ))}
-                    </DivFlex>
-                    <button onClick={() => navigate(`/search?q=latest`)}>
-                        點我看更多
-                    </button>
-                    <Title>熱門課程</Title>
-                    <DivFlex>
-                        {popularCourse.map(course => (
-                            <CourseInfo
-                                key={course.courseID}
-                                courseID={course.courseID}
-                                title={course.title}
-                                teacherName={course.teacherInfo.name}
-                                openingDate={new Date(
-                                    course.openingDate.seconds * 1000,
-                                ).toLocaleDateString()}
-                                view={course.view}
-                            />
-                        ))}
-                    </DivFlex>
-                    <button onClick={() => navigate(`/search?q=popular`)}>
-                        點我看更多
-                    </button>
-                </Div1>
-            )}
-        </Container>
+                </InputArea>
+            </Banner>
+            {/* <Container>
+                {!latestCourse || !popularCourse ? (
+                    "loading..."
+                ) : (
+                    <Div1>
+                        <Title>最新上架</Title>
+                        <DivFlex>
+                            {latestCourse.map(course => (
+                                <CourseInfo
+                                    key={course.courseID}
+                                    courseID={course.courseID}
+                                    title={course.title}
+                                    teacherName={course.teacherInfo.name}
+                                    creatDate={new Date(
+                                        course.creatTime.seconds * 1000,
+                                    ).toLocaleDateString()}
+                                    openingDate={new Date(
+                                        course.openingDate.seconds * 1000,
+                                    ).toLocaleDateString()}
+                                />
+                            ))}
+                        </DivFlex>
+                        <button onClick={() => navigate(`/search?q=latest`)}>
+                            點我看更多
+                        </button>
+                        <Title>熱門課程</Title>
+                        <DivFlex>
+                            {popularCourse.map(course => (
+                                <CourseInfo
+                                    key={course.courseID}
+                                    courseID={course.courseID}
+                                    title={course.title}
+                                    teacherName={course.teacherInfo.name}
+                                    openingDate={new Date(
+                                        course.openingDate.seconds * 1000,
+                                    ).toLocaleDateString()}
+                                    view={course.view}
+                                />
+                            ))}
+                        </DivFlex>
+                        <button onClick={() => navigate(`/search?q=popular`)}>
+                            點我看更多
+                        </button>
+                    </Div1>
+                )}
+            </Container> */}
+        </>
     );
 };
